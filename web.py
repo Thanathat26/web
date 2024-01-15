551791fb661ff071687b4b503b98a500e3a50466
Thanathat26 <154845738+Thanathat26@users.noreply.github.com>
2024-01-15 19:32:57 +0700

    debug nvbar

--- web.py
+++ web.py
@@ -1,15 +1,17 @@
-from flask import Flask,render_template,request
+from flask import Flask, render_template, request
+
 app = Flask(__name__)
-#กำหนดurl เเล้วดึงข้อมูล
+# กำหนด url แล้วดึงข้อมูล
 @app.route("/login", methods=['POST', 'GET'])
 def login():
-    if request.method =="POST":
+    if request.method == "POST":
         form = request.form['msg']
-        print("Message: %s"%(form))
+        print("Message: %s" % (form))
     return render_template("login.html")
-    
+
 @app.route("/Home")
 def Home():
     return render_template("login.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
