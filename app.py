5b459e5ce7a2adc8bb87c7904d05333035cc6279
Thanathat26 <154845738+Thanathat26@users.noreply.github.com>
2024-01-15 18:21:04 +0700

    Test POST GET

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 app = Flask(__name__)
-
-@app.route("/login")
+#กำหนดurl เเล้วดึงข้อมูล
+@app.route("/login", methods=['POST', 'GET'])
 def login():
+    if request.method =="POST":
+        form = request.form['msg']
+        print("Message: %s"%(form))
     return render_template("login.html")
-
+    
+@app.route("/Home")
+def Home():
+    return render_template("login.html")
 if __name__ == "__main__":
     app.run(debug=True)
