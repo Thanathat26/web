5ce4dc7476e3e7a5b062f8b850c0d9841979035a
Thanathat26 <154845738+Thanathat26@users.noreply.github.com>
2024-01-16 23:18:26 +0700

    add class database

--- web.py
+++ web.py
@@ -10,8 +10,14 @@ from flask_login import (
 )
 
 app = Flask(__name__)
-ab = SQLAlchemy(app)
+db = SQLAlchemy(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SECRET_KEY'] = 'thisissecretkey'
+
+class user(db.Model,UserMixin):
+    id = db.Column(db.Integer,primary_key=True)
+    username = db.Column(db.String(20),nullable=False)
+    password = db.Column(db.String(20),nullable=False)
 # กำหนด url แล้วดึงข้อมูล
 #login
 @app.route("/login")
