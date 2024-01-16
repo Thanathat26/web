769006ff7d1eef6eb5243778e9be6035a3a0197a
Thanathat26 <154845738+Thanathat26@users.noreply.github.com>
2024-01-16 23:57:51 +0700

    fix bug and register class

--- web.py
+++ web.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from flask import redirect
+from wtforms import Form, StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError
 from flask_login import (
     LoginManager,
     login_required,
@@ -10,14 +13,21 @@ from flask_login import (
 )
 
 app = Flask(__name__)
-db = SQLAlchemy(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY'] = 'thisissecretkey'
-
+db = SQLAlchemy(app)
 class user(db.Model,UserMixin):
     id = db.Column(db.Integer,primary_key=True)
     username = db.Column(db.String(20),nullable=False)
-    password = db.Column(db.String(20),nullable=False)
+    password = db.Column(db.String(80),nullable=False)
+class RegisterForm(FlaskForm):
+    username = StringField(validators=[
+                           InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+
+    password = PasswordField(validators=[
+                             InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
+
+    submit = SubmitField('Register')
 # กำหนด url แล้วดึงข้อมูล
 #login
 @app.route("/login")
@@ -25,12 +35,12 @@ def login():
     return render_template("login.html")
 #registr
 @app.route("/register")
-def Home():
+def register():
     return render_template("register.html")
 #home
 @app.route("/home")
 def Home():
     return render_template("home.html")
-#
+#run
 if __name__ == "__main__":
     app.run(debug=True)
