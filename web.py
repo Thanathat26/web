966ec5252b9390700faa4664d9f6652a8cb072a0
Thanathat26 <154845738+Thanathat26@users.noreply.github.com>
2024-01-17 00:03:11 +0700

    login + get post

--- web.py
+++ web.py
@@ -17,6 +17,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY'] = 'thisissecretkey'
 db = SQLAlchemy(app)
+#ไม่เอาgit คนอื่นทั้งดุ้น
 class user(db.Model,UserMixin):
     id = db.Column(db.Integer,primary_key=True)
     username = db.Column(db.String(20),nullable=False)
@@ -29,9 +30,24 @@ class RegisterForm(FlaskForm):
                              InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
 
     submit = SubmitField('Register')
+    def validate_username(self, username):
+        existing_user_username = User.query.filter_by(
+            username=username.data).first()
+        if existing_user_username:
+            raise ValidationError(
+                'That username already exists. Please choose a different one.')
+    
+class LoginForm(FlaskForm):
+    username = StringField(validators=[
+                           InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+
+    password = PasswordField(validators=[
+                             InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
+
+    submit = SubmitField('Login')
 # กำหนด url แล้วดึงข้อมูล
 #login
-@app.route("/login")
+@app.route("/login",methods=['GET','POST'])
 def login():
     return render_template("login.html")
 #registr
