1d7e9d4ba6eab22c29016cffa62a6091671959ba
Thanathat26 <154845738+Thanathat26@users.noreply.github.com>
2024-01-30 21:16:10 +0700

    home 1.2

--- web.py
+++ web.py
@@ -14,7 +14,6 @@ from flask_login import (
 )
 from flask_mysqldb import MySQL
 app = Flask(__name__)
-app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'mydatabase'
